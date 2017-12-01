5836fce191055c5c39e59ba162b380aff87056ad
Ben Tranter <ben@bentranter.io>
2017-12-01 17:23:34 -0500

    Add test case for non-existent template
    Ensures that an error occurs when you attempt to render a template that
    doesn't exist.

--- turbo_test.go
+++ turbo_test.go
@@ -11,12 +11,15 @@ import (
 func TestTurbo(t *testing.T) {
 	render := turbo.New("fixtures/basic", "layout")
 
-	t.Run("package test", func(t *testing.T) {
+	t.Run("render template without errors", func(t *testing.T) {
 		const expected = `head<p>test</p>foot`
 		var err error
 		h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			err = render.HTML(w, http.StatusOK, "content", "test")
 		})
+		if err != nil {
+			t.Fatalf("unexpected error rendering template: %v", err)
+		}
 
 		res := httptest.NewRecorder()
 		req, _ := http.NewRequest("GET", "/", nil)
@@ -28,6 +31,24 @@ func TestTurbo(t *testing.T) {
 		body := res.Body.String()
 		if body != expected {
 			t.Fatalf("expected %s but got %s", expected, body)
+		}
+	})
+
+	t.Run("render unnamed template", func(t *testing.T) {
+		var err error
+		h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			err = render.HTML(w, http.StatusOK, "notFound", "test")
+		})
+		if err != nil {
+			t.Fatalf("expecting error when rendering non-existent template but got nil")
+		}
+
+		res := httptest.NewRecorder()
+		req, _ := http.NewRequest("GET", "/", nil)
+		h.ServeHTTP(res, req)
+
+		if res.Code == http.StatusInternalServerError {
+			t.Fatalf("expected HTTP status %d but got %d", http.StatusInternalServerError, res.Code)
 		}
 	})
 }
